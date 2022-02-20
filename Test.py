c357646852373000ff14610c9f2577bfc799d348
RonDiamse <rondiamse@yahoo.com>
2022-02-21 04:49:03 +0800

    upload Test.py

---
+++ Test.py
@@ -0,0 +1,11 @@
+print("Hello World")
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello "+ name + "! You are " + age)
+
+
